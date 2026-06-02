5df720d552f1e69c9a9801e9c744ee2a15598402
shekhar <shekharjamalpuri18@gmail.com>
2026-06-02 15:33:24 +0530

    Day 2 Two pointers

---
+++ Two_Pointers/Trapping_Rain_Water.py
@@ -0,0 +1,21 @@
+def trap(height):
+    lmax = 0
+    rmax = 0
+    total = 0
+    l = 0
+    r = len(height) - 1
+    while l < r:
+        if height[l] <= height[r]:
+            if lmax > height[l]:
+                total += lmax - height[l]
+            else:
+                lmax = height[l]
+            l += 1
+        else:
+            if rmax > height[r]:
+                total += rmax - height[r]
+            else:
+                rmax = height[r]
+            r -= 1
+    return total
+print(trap(height = [0,1,0,2,1,0,1,3,2,1,2,1]))
---
+++ Two_Pointers/Two_sum_sorted.py
@@ -0,0 +1,13 @@
+def twoSum(numbers, target):
+    l = 0
+    r = len(numbers) - 1
+    while l < r:
+        s = numbers[l] + numbers[r]
+        if s == target:
+            return [l+1,r+1]
+        elif s < target:
+            l += 1
+        else:
+            r -= 1
+    return [-1,-1]
+print(twoSum([2,7,11,13],9))
